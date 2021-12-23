c120032d1e33b5d2899220f3deb91efee2bab3ec
ivanfsilva <ivanfs27@gmail.com>
2021-12-22 21:15:47 -0300

    findById for Category

--- backend/src/main/java/br/com/ivanfsilva/bookstore/domain/Livro.java
+++ backend/src/main/java/br/com/ivanfsilva/bookstore/domain/Livro.java
@@ -1,5 +1,7 @@
 package br.com.ivanfsilva.bookstore.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.persistence.*;
 
 import java.io.Serializable;
@@ -18,6 +20,7 @@ public class Livro implements Serializable {
     private String texto;
 
 
+    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
---
+++ backend/src/main/java/br/com/ivanfsilva/bookstore/resources/CategoriaResource.java
@@ -0,0 +1,24 @@
+package br.com.ivanfsilva.bookstore.resources;
+
+import br.com.ivanfsilva.bookstore.domain.Categoria;
+import br.com.ivanfsilva.bookstore.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @Autowired
+    CategoriaService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
+        Categoria obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
