fbbaf4ce40678351b0398804f3ddef402fc6a9ea
ivanfsilva <ivanfs27@gmail.com>
2022-01-05 23:59:43 -0300

    category-read added

---
+++ frontend/src/app/components/views/categoria/categoria-read/categoria-read.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from "../categoria.service";
+import { Categoria } from "../categoria.model";
+
+@Component({
+  selector: 'app-categoria-read',
+  templateUrl: './categoria-read.component.html',
+  styleUrls: ['./categoria-read.component.css']
+})
+export class CategoriaReadComponent implements OnInit {
+
+  categorias: Categoria[] = [];
+  displayedColumns: string[] = ['id', 'nome', 'descricao', 'livros', 'acoes'];
+
+  constructor( private service: CategoriaService ) { }
+
+  ngOnInit(): void {
+    this.findAll();
+  }
+
+  findAll():void {
+    this.service.findAll().subscribe(resposta => {
+      this.categorias = resposta;
+    });
+  }
+}
